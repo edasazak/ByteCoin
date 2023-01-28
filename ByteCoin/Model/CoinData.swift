e8e57f930b3e6896d479034e8256496b1cec6b4f
edasazak <edasazak29@gmail.com>
2023-01-28 16:26:33 +0300

    first commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Mac on 28.01.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Decodable {
+    let rate: Double
+}
